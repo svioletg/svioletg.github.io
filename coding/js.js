626ee588662bca67630d2045aedb855c1d48b416
Alphys Hedge <ultimategfbcfan@gmail.com>
2019-05-12 08:55:45 -0500

    Add posts subsection

--- coding/js.js
+++ coding/js.js
@@ -4,6 +4,16 @@ $('td:contains("No")').addClass('no');
 $('td:contains("Yes*")').toggleClass('yes asterisk');
 $('td:contains("No*")').toggleClass('no asterisk');
 
+//  1 = dark
+// -1 = light
+var theme = 1;
+
+$('.themetoggle').click(function(){
+	theme*=-1;
+	if (theme == 1) {$(this).html('LIGHT THEME'); $('div#page').removeClass('light');}
+	if (theme == -1) {$(this).html('DARK THEME'); $('div#page').addClass('light');}
+})
+
 // Sorting function: https://stackoverflow.com/a/49041392/8108924
 
 const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
