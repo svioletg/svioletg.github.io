a8012f917befe6383d6b96b70836f933a00adf2a
Alphys Hedge <ultimategfbcfan@gmail.com>
2019-08-02 19:33:18 -0500

    Footer update

--- footer.js
+++ footer.js
@@ -1,1 +1,7 @@
-$('span#devcredit').html('Website written & created by: Seth Gibbs<a href="https://github.com/Alpha-Hedge"><img src="https://img.icons8.com/color/32/000000/github.png"></a><br>Contact / email: seth@sethgibbs.xyz<br><a href="https://icons8.com/icon/63777/github">GitHub icon by Icons8</a>')
+$('span#devcredit').html(
+	`Website written & created by Seth Gibbs<a href="https://github.com/Alpha-Hedge"><img src="https://img.icons8.com/color/32/000000/github.png"></a>
+	<br>Contact / email: seth@sethgibbs.xyz
+	<br><a href="https://icons8.com/icon/63777/github">GitHub icon by Icons8</a>
+	<br><a href="https://icons8.com/icon/63308/info">Info icon by Icons8</a>
+	<br>Menu icon made by <a href="https://www.flaticon.com/authors/cole-bemis" title="Cole Bemis">Cole Bemis</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>`
+)
