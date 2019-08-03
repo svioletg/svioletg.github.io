fba77c8472471bf52277e7426f9aa0e20caaf248
Alphys Hedge <ultimategfbcfan@gmail.com>
2019-08-02 19:30:33 -0500

    Track page style testing

---
+++ music/track/js.js
@@ -0,0 +1,15 @@
+$('button.bttn-a').click(function(){
+	$('body').removeClass().addClass('palette-a');
+})
+
+$('button.bttn-b').click(function(){
+	$('body').removeClass().addClass('palette-b');
+})
+
+$('button.bttn-c').click(function(){
+	$('body').removeClass().addClass('palette-c');
+})
+
+$('button.bttn-d').click(function(){
+	$('body').removeClass().addClass('palette-d');
+})
