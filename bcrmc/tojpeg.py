5dab1a1586f5a17266724a3d6c631a7d41f1cfe6
Seth Gibbs <16344364+svioletg@users.noreply.github.com>
2023-03-03 01:06:43 -0600

    more photos

---
+++ bcrmc/tojpeg.py
@@ -0,0 +1,19 @@
+import inquirer
+import os, shutil, glob
+
+q = [inquirer.List('folder', message='Choose image root folder', choices=['bcr1', 'bcr2', 'bcr3', 'bcr4'], carousel=True)]
+folder = inquirer.prompt(q)['folder']
+
+pngs = os.listdir(f'{folder}/png')
+jpgs = os.listdir(f'{folder}/jpg')
+
+pngs = [i.split('.')[0] for i in pngs]
+jpgs = [i.split('.')[0] for i in jpgs]
+
+diff = list(set(pngs) - set(jpgs))
+
+for i in diff:
+	os.system(f'ffmpeg -i {folder}/png/{i}.png {folder}/jpg/{i}.jpg -q:v 0 -loglevel error')
+	print(i)
+
+input('Done. Press ENTER to continue.')
