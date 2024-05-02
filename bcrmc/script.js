8689180791992f564526f24741f64e66a11c84de
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2024-05-02 18:54:53 -0500

    restructuring, auto theming, stufffff

--- bcrmc/script.js
+++ bcrmc/script.js
@@ -11,33 +11,4 @@ function create_section_anchors()
     }
 }
 
-function apply_correct_gallery_header_backgrounds()
-{
-    let tags_h2 = document.getElementsByTagName('h2');
-    for (let i = 0; i < tags_h2.length; i++) 
-    {
-        let tag = tags_h2[i];
-        switch (tag.id) {
-            case '114':
-                tag.style.backgroundImage = "url('i/slate_128x128.png')";
-                break;
-            case '115':
-                tag.style.backgroundImage = "url('i/honeycomb_block_64x64_dark.png')";
-                break;
-            case '118':
-                tag.style.backgroundImage = "url('i/slate_128x128.png')";
-                break;
-            case '119':
-                tag.style.backgroundImage = "url('i/sculk_catalyst_top_128x128.png')";
-                break;
-            case '120':
-                tag.style.backgroundImage = "url('i/cherry_planks_128x128.png')";
-                break;
-            default:
-                break;
-        }
-    }
-}
-
 create_section_anchors();
-apply_correct_gallery_header_backgrounds();
