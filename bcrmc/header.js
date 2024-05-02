fe977c0521a197c1f26c1858e801a7477d97aef6
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2024-05-02 18:01:31 -0500

    middle of big restructuring

---
+++ bcrmc/header.js
@@ -0,0 +1,19 @@
+function add_icon(header) {
+    header.innerHTML += "<link rel=\"icon\" type=\"image/x-icon\" href=\"/bcrmc/favicon.ico\">";
+}
+function add_viewport_meta(header) {
+    header.innerHTML += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">";
+}
+function main() {
+    var header = document.getElementsByTagName("head")[0];
+    console.log(header);
+    if (!header.innerHTML.includes("href=\"favicon.ico\"")) {
+        console.log("Adding icon");
+        add_icon(header);
+    }
+    if (!header.innerHTML.includes("meta name=\"viewport\"")) {
+        console.log("Adding viewport");
+        add_viewport_meta(header);
+    }
+}
+main();
