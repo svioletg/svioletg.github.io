b665d97b65a0254f52da5f9da2a66dc7c54482c9
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2024-05-02 15:56:41 -0500

    will try to convert my python for this

---
+++ bcrmc/header.ts
@@ -0,0 +1,23 @@
+function add_icon(header: HTMLHeadElement): void {
+    header.innerHTML += `<link rel="icon" type="image/x-icon" href="favicon.ico">`;
+}
+
+function add_viewport_meta(header: HTMLHeadElement): void {
+    header.innerHTML += `<meta name="viewport" content="width=device-width, initial-scale=1.0">`;
+}
+
+function main(): void {
+    let header: HTMLHeadElement = document.getElementsByTagName("head")[0];
+    console.log(header);
+    if (!header.innerHTML.includes(`href="favicon.ico"`)) {
+        console.log("Adding icon");
+        add_icon(header);
+    }
+
+    if (!header.innerHTML.includes(`meta name="viewport"`)) {
+        console.log("Adding viewport");
+        add_viewport_meta(header);
+    }
+}
+
+main()
