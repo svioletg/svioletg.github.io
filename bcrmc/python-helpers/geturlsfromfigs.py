8028d947673ddbe896846060ef1fe6008a6faf8f
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2024-05-04 00:50:41 -0500

    quick patch, old galleries are there for debugging just in case

--- bcrmc/python-helpers/geturlsfromfigs.py
+++ bcrmc/python-helpers/geturlsfromfigs.py
@@ -1,21 +1,25 @@
 import re
 import json
+import os
 
 from pathlib import Path
 
-pat = re.compile(r"""<figure.*url\('.*?/(.*)'\).*?<figcaption>(.*?[ '\w]*)\W""")
+pat = re.compile(r"""<figure.*url\('.*?/(.*)'\).*?<figcaption>(.*?[^<]+)""")
 
 out = {}
 
 for i in range(5):
-    out[f'bcr{i+1}'] = {}
-    with open(Path(f'bcr{i+1}/gallery.html'), encoding='utf-8') as f:
+    out[f'bcr{i + 1}'] = {}
+    with open(Path(f'gallery{i + 1}.html'), encoding='utf-8') as f:
         html = f.read()
         matches = re.findall(pat, html)
         if matches:
             for m in matches:
-                out[f'bcr{i+1}'][m[0]] = m[1]
+                filename = Path(m[0]).stem
+                out[f'bcr{i + 1}'][Path(filename).stem] = {
+                    'title': m[1].strip(),
+                    'has_png': Path(f'../bcr{i + 1}/png/{filename}.png').exists()
+                }
 
-print(out)
 with open('_out.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(out, indent=4))
