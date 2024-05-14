161ca5533396110fa5b056ee9cff4ff66b55c058
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2024-05-14 02:29:58 -0500

    interactive maps have been moved to another server
    otherwise it just clogs the hell out of git

--- bcrmc/gallery/interactive/map-redirect.js
+++
@@ -1,4 +0,0 @@
-// Main
-var params = new URLSearchParams(window.location.search);
-var target_map = params.get('d') || 'overworld';
-window.location.href = window.location.href.split('?')[0] + "".concat(target_map, "/");
--- bcrmc/gallery/interactive/map-redirect.ts
+++
@@ -1,5 +0,0 @@
-// Main
-let params = new URLSearchParams(window.location.search);
-let target_map: string = params.get('d') || 'overworld';
-
-window.location.href = window.location.href.split('?')[0] + `${target_map}/`;
