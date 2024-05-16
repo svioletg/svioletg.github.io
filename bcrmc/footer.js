f09a9043d6cfa597506fdca044ac947213620fee
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2024-05-16 13:11:19 -0500

    auto footer

--- bcrmc/footer.js
+++ bcrmc/footer.js
@@ -1,3 +1,12 @@
-import { $ } from "./utils.js";
-const FOOTER = $('footer');
+const FOOTER = document.querySelector('footer');
 const CONTENT = FOOTER.appendChild(document.createElement('div'));
+let footer_html = `
+    <hr>
+    <div>
+    <p>{ Website written by <a href="https://github.com/svioletg">Seth "Violet" Gibbs</a> }</p>
+    <p>{ If you're having any problems with the site, you can <a href="https://github.com/svioletg/svioletg.github.io/issues">submit an issue</a>
+    if you have a GitHub account, or let me know directly. }</p>
+    </div>
+    <hr>
+`;
+CONTENT.innerHTML = footer_html;
--- bcrmc/footer.ts
+++ bcrmc/footer.ts
@@ -1,5 +1,15 @@
-import { $ } from "./utils.js";
-
-const FOOTER: HTMLElement = $('footer');
+const FOOTER: HTMLElement = document.querySelector('footer');
 
 const CONTENT: HTMLDivElement = FOOTER.appendChild(document.createElement('div'));
+
+let footer_html: string = `
+    <hr>
+    <div>
+    <p>{ Website written by <a href="https://github.com/svioletg">Seth "Violet" Gibbs</a> }</p>
+    <p>{ If you're having any problems with the site, you can <a href="https://github.com/svioletg/svioletg.github.io/issues">submit an issue</a>
+    if you have a GitHub account, or let me know directly. }</p>
+    </div>
+    <hr>
+`;
+
+CONTENT.innerHTML = footer_html;
