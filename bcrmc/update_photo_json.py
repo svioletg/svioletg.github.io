e7ef33be3dd9c3de4c24911ad6f54a9acc7846b6
Seth "Violet" Gibbs <16344364+svioletg@users.noreply.github.com>
2024-09-26 17:48:33 -0500

    couple script fixes

--- bcrmc/update_photo_json.py
+++ bcrmc/update_photo_json.py
@@ -16,30 +16,53 @@ def main():
     root_png = root / 'png'
     root_jpg = root / 'jpg'
 
-    png: list[str] = [f.stem for f in map(Path, root_png.iterdir()) if f.suffix == '.png']
-    jpg: list[str] = [f.stem for f in map(Path, root_jpg.iterdir()) if f.suffix == '.jpg']
+    png: list[str] = [f.stem for f in map(Path, root_png.glob('*.png'))]
+    jpg: list[str] = [f.stem for f in map(Path, root_jpg.glob('*.jpg'))]
 
     files = set(jpg + png)
 
     with open('photo_authors.json', 'r', encoding='utf-8') as f:
         authors: dict[str, str] = json.load(f)
     with open('photo_titles.json', 'r', encoding='utf-8') as f:
-        titles: dict[str, dict[str, dict[str, str]]] = json.load(f)
+        photo_data: dict[str, dict[str, dict[str, str | bool]]] = json.load(f)
 
     new_authors: list[str] = []
 
     for img in tqdm(files):
+        if img not in jpg:
+            tqdm.write(f'[!] Image not found in JPG folder: "{img}"')
+            tqdm.write('Gallery images must exist in JPG format, you may need to run galleryjpgs.py first.')
+            tqdm.write('Quitting...')
+            return
         if (author := img.split('-')[0]) not in authors:
+            tqdm.write(f'Author "{author}" not recognized, adding as new.')
             new_authors.append(author)
             authors[author] = author
-        # TODO
+        if img not in photo_data[root.stem]:
+            tqdm.write(f'New image discovered: "{img}"')
+            photo_data[root.stem][img] = {
+                'title': img,
+                'has_png': img in png
+            }
+        else:
+            tqdm.write(f'Image already in JSON: "{img}"')
 
     if new_authors:
         print(f'{len(new_authors)} new authors were added to JSON:')
         print('\n  - '.join(new_authors))
 
-    with open('photo_authors.json', 'w', encoding='utf-8') as f:
-        json.dump(authors, f, indent=4)
+    if input('Save new authors to JSON? ').lower() in ('y', 'yes'):
+        with open('photo_authors.json', 'w', encoding='utf-8') as f:
+            json.dump(authors, f, indent=4)
+        print('Done.')
+    else:
+        print('No changes made to disk.')
+
+    if input('Save new images to JSON? ').lower() in ('y', 'yes'):
+        with open('photo_titles.json', 'w', encoding='utf-8') as f:
+            json.dump(photo_data, f, indent=4)
+    else:
+        print('No changes made to disk.')
 
 if __name__ == '__main__':
     main()
